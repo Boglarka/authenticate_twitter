00482978eed2c46924c4e803338405ea10b3c7b3
Boglarka <weltz.boglarka@gmail.com>
2013-12-03 19:02:39 +0100

    Started authentication-over-Twitter app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthenticateOverTwitter::Application.config.secret_key_base = '42f4969272e6c837d0028abe455c0b9de66e81d4c28002ceb47f899771d195db7a8d47f176063b86b3af7b9c4ae2104bb6c21b0f6f158770abe877e57cc126ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthenticateOverTwitter::Application.config.session_store :cookie_store, key: '_authenticate-over-twitter_session'
